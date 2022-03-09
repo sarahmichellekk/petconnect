79c8c096adfb4a186a3c1c88d940d4f27822bec8
Abbey Drake <drakeam1189@gmail.com>
2022-03-09 15:26:38 -0700

    added renderPetInfoCard and FlatList to PetInfoComponent

--- components/PetInfoComponent.js
+++ components/PetInfoComponent.js
@@ -1,38 +1,30 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
-import { Card } from "react-native-elements";
-import { FlatList } from "react-native-gesture-handler";
+import { Text, View, FlatList } from "react-native";
+import { Card, ListItem } from "react-native-elements";
 import { CATEGORIES } from "../shared/categories";
-import { DOGS } from "../shared/dogs";
+import { ANIMALS } from "../shared/animals";
 
-function RenderPetInfo({ category }) {
+function RenderPetInfo(props) {
+  const {category} = props;
   if (category) {
     return (
-      <Card featuredTitle={category.name} image={require("./images/lucy.jpg")}>
-        <Text style={{ margin: 10 }}>{category.description}</Text>
-      </Card>
+      <View>
+        <Card featuredTitle={category.name} image={require("./images/lucy.jpg")}>
+          <Text style={{ margin: 10 }}>{category.description}</Text>
+        </Card>
+      </View>
     );
   }
   return <View />;
 }
 
-// function RenderDogs({dog}) {
-//   if(dog) {
-//     return (
-//       <Card
-//         featuredTitle={dog.name}
-//         image={require{dog.name}}>
-//       </Card>
-//     );
-//   }
-// }
 
 class PetInfo extends Component {
   constructor(props) {
     super(props);
     this.state = {
       categories: CATEGORIES,
-      dogs: DOGS
+      animals: ANIMALS
     };
   }
 
@@ -40,16 +32,30 @@ class PetInfo extends Component {
     title: "Pet Information",
   };
 
+
   render() {
     const petId = this.props.navigation.getParam("petId");
     const category = this.state.categories.filter(
       (category) => category.id === petId
     )[0];
-    const dog = this.state.dogs.filter((dog) => dog.id === petId[0]);
+
+    const renderPetInfoCard = ({ item }) => {
+      return (
+        <ListItem
+          title={item.name}
+          leftAvatar={{ source: require("./images/pete.jpg") }}
+        />
+      );
+    };
+
     return (
    <View>
-       <RenderPetInfo category={category} />
-        {/* <RenderDogs dog={dog} /> */}
+       <RenderPetInfo category={category}/>
+       <FlatList
+        data={this.state.animals}
+        renderItem={renderPetInfoCard}
+        keyExtractor={(item) => item.id.toString()}
+      />
    </View>
     
     );
---
+++ shared/animals.js
@@ -0,0 +1,140 @@
+export const ANIMALS = [
+    {
+        id: 0,
+        name: "Freddy",
+        type: "Birds",
+        image: "./components/images/freddy.jpg"
+    },
+    {
+        id: 1,
+        name: "Patricia",
+        type: "Birds",
+        image: "./components/images/patricia.jpg"
+    },
+    {
+        id: 2,
+        name: "Pete",
+        type: "Birds",
+        image: "./components/images/pete.jpg"
+    },
+    {
+        id: 3,
+        name: "Sid",
+        type: "Birds",
+        image: "./components/images/sid.jpg"
+    },
+    {
+        id: 4,
+        name: "Tally",
+        type: "Birds",
+        image: "./components/images/tally.jpg"
+    },
+    {
+        id: 5,
+        name: "Chester",
+        type: "Cats",
+        image: "./components/images/chester.jpg"
+    },
+    {
+        id: 6,
+        name: "Gracie",
+        type: "Cats",
+        image: "./components/images/gracie.jpg"
+    },
+    {
+        id: 7,
+        name: "Leah",
+        type: "Cats",
+        image: "./components/images/leah.jpg"
+    },
+    {
+        id: 8,
+        name: "Princess Caraboo",
+        type: "Cats",
+        image: "./components/images/princesscaraboo.jpg"
+    },
+    {
+        id: 9,
+        name: "Janis",
+        type: "Cats",
+        image: "./components/images/janis.jpg"
+    },
+    {
+        id: 10,
+        name: "Sir Hank",
+        type: "Cats",
+        image: "./components/images/sirhank.jpg"
+    },
+    {
+        id: 11,
+        name: "Bear",
+        type: "Dogs",
+        image: "./components/images/bear.jpg"
+    },
+    {
+        id: 12,
+        name: "Benji",
+        type: "Dogs",
+        image: "./components/images/benji.jpg"
+    },
+    {
+        id: 13,
+        name: "Chance",
+        type: "Dogs",
+        image: "./components/images/chance.jpg"
+    },
+    {
+        id: 14,
+        name: "Doug",
+        type: "Dogs",
+        image: "./components/images/doug.jpg"
+    },
+    {
+        id: 15,
+        name: "Janis",
+        type: "Dogs",
+        image: "./components/images/janis.jpg"
+    },
+    {
+        id: 16,
+        name: "Lucy",
+        type: "Dogs",
+        image: "./components/images/lucy.jpg"
+    },
+    {
+        id: 17,
+        name: "Sweetie",
+        type: "Dogs",
+        image: "./components/images/sweetie.jpg"
+    },
+    {
+        id: 18,
+        name: "Sweetie",
+        type: "Dogs",
+        image: "./components/images/sweetie.jpg"
+    },
+    {
+        id: 19,
+        name: "Fiona",
+        type: "Reptiles",
+        image: "./components/images/fiona.jpg"
+    },
+    {
+        id: 20,
+        name: "Gabe",
+        type: "Reptiles",
+        image: "./components/images/gabe.jpg"
+    },
+    {
+        id: 21,
+        name: "Pebbles",
+        type: "Reptiles",
+        image: "./components/images/pebbles.jpg"
+    },
+    {
+        id: 22,
+        name: "Sam",
+        type: "Reptiles",
+        image: "./components/images/sam.jpg"
+    }
+]
