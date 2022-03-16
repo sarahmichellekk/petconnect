0cf026f0ee4fc26283ce61166a79685a85e65165
Abbey Drake <drakeam1189@gmail.com>
2022-03-16 15:57:06 -0600

    added descriptions

--- shared/animals.js
+++ shared/animals.js
@@ -14,7 +14,7 @@ export const ANIMALS = [
         type: "Birds",
         image: require('../components/images/patricia.jpg'),
         featured: false,
-        description: "Your family's eyes will sparkle at the first sight of me!",
+        description: "The Alexandrine parakeet is one of the largest parakeets, measuring 56 to 62 cm (22 to 24 in) from the top of the head to the tip of the tail and weighing 200 to 300 g (7.1 to 10.6 oz). The tail measures 28 to 35 cm (11 to 14 in). It is predominantly green with a light blue-grey sheen on the cheeks and nape (back of the neck), yellow-green abdomen, red patch on the shoulders and massive red beak with yellow tips. The upper-side of the tail passes from green at the top to blue further down, and is yellow at the tip. The underside of the tail is yellow",
         agency: "Denver Animal Shelter"
     },
     {
@@ -32,7 +32,9 @@ export const ANIMALS = [
         type: "Birds",
         image: require('../components/images/sid.jpg'),
         featured: false,
+        description: "Your family's eyes will sparkle at the first sight of me! I'm a friendly fellow and love being around people.",
         agency: "Denver Animal Shelter"
+        
     },
     {
         id: 4,
@@ -40,6 +42,7 @@ export const ANIMALS = [
         type: "Birds",
         image: require('../components/images/tally.jpg'),
         featured: false,
+        description: "Her bright colors will dazzle you! In the wild, this species travels in flocks ranging in size from about four to over a hundred birds. Most species travel in flocks of about five to forty. They form lifelong, tight pair bonds with their chosen mates.",
         agency: "Exotic Birds of Denver"
 
     },
@@ -48,6 +51,7 @@ export const ANIMALS = [
         name: "Chester",
         type: "Cats",
         image: require('../components/images/chester.jpg'),
+        description: "Hi everyone! I enjoy chatting, hunting my wand and fluff balls, sitting in your lap for a nap, hiding behind drapes waiting for you to find me, and sleeping in your bed or on my tower in the window.",
         featured: false
     },
     {
@@ -55,6 +59,7 @@ export const ANIMALS = [
         name: "Gracie",
         type: "Cats",
         image: require('../components/images/gracie.jpg'),
+        description: "My name is Gracie and I'm a sweet shy girl looking for my forever home. I'm still pretty timid when it comes to people, but OH BOY do I love other cats! I would really do well with another cat who wants to be as cuddly as I am with them. I'm also REALLY good at giving them head bumps! My new cat friends here in my foster home have really brought me out of my shell and are showing me that people really aren't so bad!",
         featured: false
     },
     {
@@ -62,6 +67,7 @@ export const ANIMALS = [
         name: "Leah",
         type: "Cats",
         image: require('../components/images/leah.jpg'),
+        description: "Leah is a sweetheart and total foodie! Her favorite treats include Weruva Mack and Jack flavored wet food as well as Science Diet.",
         featured: true 
     },
     {
@@ -69,105 +75,106 @@ export const ANIMALS = [
         name: "Princess Caraboo",
         type: "Cats",
         image: require('../components/images/princesscaraboo.jpg'),
+        description:"Princess Caraboo is a sweet girl with the most beautiful green eyes you've probably ever seen. She needs time to warm up in a new space and ideally a place to hide. After a few days, she warms right up and is attentive to what's going on around her. She absolutely melts when you pet her, and she would do well with kind-hearted people that will just love on her. Her favorite thing to chase is reflections/lights along the wall and she likes to tackle her sister Arizona.",
         featured: false
     },
+    
     {
         id: 9,
-        name: "Janis",
+        name: "Sir Hank",
         type: "Cats",
-        image: require('../components/images/janis.jpg'),
+        image: require('../components/images/sirhank.jpg'),
+        description:" Once I know you, I will follow you from room to room - especially to the kitchen! I prefer to have my own space most of the time but when I need some love I am not afraid to come ask for it, and I will always sleep at your legs at night. I am still on the fence about being held, but we continue to work on it.",
         featured: false
     },
     {
         id: 10,
-        name: "Sir Hank",
-        type: "Cats",
-        image: require('../components/images/sirhank.jpg'),
+        name: "Bear",
+        type: "Dogs",
+        image: require('../components/images/bear.jpg'),
+        description: "Bear is a curious youngster but can be a bit shy when meeting new people for the first time. With time she warms up and is ready to explore, sniff, and play. She's skittish being handled around her collar and neck so go slow with leashing! She'll do best with an experienced owner who will provide ongoing positive reinforcement training and she would like to meet all family members and any other dogs in the home. ",
         featured: false
     },
     {
         id: 11,
-        name: "Bear",
+        name: "Benji",
         type: "Dogs",
-        image: require('../components/images/bear.jpg'),
+        image: require('../components/images/benji.jpg'),
+        description: "Benji has a big personality! He’s a loveable, rambunctious, playful, mouthy youngster. He’s full of puppy energy and will need plenty of outlets to run, play, and explore. Tyson will do best in a home with experienced, active owners who will provide lots of consistent positive reinforcement training so that he can learn good grownup manners and be his best self. ",
         featured: false
     },
     {
         id: 12,
-        name: "Benji",
+        name: "Chance",
         type: "Dogs",
-        image: require('../components/images/benji.jpg'),
+        image: require('../components/images/chance.jpg'),
+        description: "He is a friendly guy who is easy to leash and is a strong walker who would benefit from having a harness for his walks. While out and about he is an explorer and a sniffer. He is a smart guy who knows the sit and stay commands. Bring in your whole family and we recommend older kids in the family. ",
         featured: false
     },
     {
         id: 13,
-        name: "Chance",
+        name: "Doug",
         type: "Dogs",
-        image: require('../components/images/chance.jpg'),
+        image: require('../components/images/doug.jpg'),
+        description: "I came here as a transfer all the way from Oklahoma and so far have been loving my Colorado experience. I’m a goofy guy who loves to snuggle and give affection, I even get my gums stuck sometime and end up giving a big fun grin 🙂 I also am a bit aloof at times and do my own thing but generally am a playful cuddly love bug!",
         featured: false
     },
     {
         id: 14,
-        name: "Doug",
-        type: "Dogs",
-        image: require('../components/images/doug.jpg'),
-        featured: false
-    },
-    {
-        id: 15,
         name: "Janis",
         type: "Dogs",
         image: require('../components/images/janis.jpg'),
+        description: "Meet Janis– a loving, energetic ball of buoyant wonder! As expected of a young dog, she LOVES to play and would benefit from plenty of playtime and exercise, as well as lots of hard chew toys that she can really get her jaws around! Previous owner states that she knows some commands and is housebroken. ",
         featured: true 
     
     },
     {
-        id: 16,
+        id: 15,
         name: "Lucy",
         type: "Dogs",
         image: require('../components/images/lucy.jpg'),
+        description: "She’s a sweet girl who takes some time to warm up, but once she does, she loves playing! She will need some training when it comes to walking on leash, and she’d appreciate a patient owner willing to help her with that. She does have a history of living with children, but has a tendency to play a little rough with them. She has not been around cats, but has lived with another dog previously. Bring the family in to meet with her today.",
         featured: false
     },
     {
-        id: 17,
+        id: 16,
         name: "Sweetie",
         type: "Dogs",
         image: require('../components/images/sweetie.jpg'),
+        description: "Looking for a cuddly dog? Meet with Sweetie! In her previous home, this gal has gotten along very well with all people as well as both other dogs and cats that she has lived with in the past. The previous owner did note some level of food aggression with this dog snarling towards other dogs whenever food is near. Because of this, we would have one of our behvavior technicians speak with you to discuss tips and tricks on how to manage this issue. If she sounds like a match for your home, please meet with Sweetie!",
+        featured: false
+    },
+   
+    {
+        id: 17,
+        name: "Fiona",
+        type: "Reptiles",
+        image: require('../components/images/fiona.jpg'),
+        description: "I eat great, and am very chill and friendly! I am very adaptable and would make a great addition to your family!",
         featured: false
     },
     {
         id: 18,
-        name: "Sweetie",
-        type: "Dogs",
-        image: require('../components/images/sweetie.jpg'),
+        name: "Gabe",
+        type: "Reptiles",
+        image: require('../components/images/gabe.jpg'),
+        description: "Gabe is a younger adult with a great appetite. This larger Leopard Gecko is great fun to watch -- but he does not enjoy being handled.",
         featured: false
     },
     {
         id: 19,
-        name: "Fiona",
+        name: "Pebbles",
         type: "Reptiles",
-        image: require('../components/images/fiona.jpg'),
+        image: require('../components/images/pebbles.jpg'),
+        description: "Anyone can get Pebbles out of his shell! Friendly dude who loves being handled. Healthy appetite",
         featured: false
     },
     {
         id: 20,
-        name: "Gabe",
-        type: "Reptiles",
-        image: require('../components/images/gabe.jpg'),
-        featured: false
-    },
-    {
-        id: 21,
-        name: "Pebbles",
-        type: "Reptiles",
-        image: require('../components/images/pebbles.jpg'),
-        featured: false
-    },
-    {
-        id: 22,
         name: "Sam",
         type: "Reptiles",
         image: require('../components/images/sam.jpg'),
+        description: "A younger Ball Python, Ann Chapman has some interesting patterning in a normal colors. Great appetite!",
         featured: false
     }
 ]
